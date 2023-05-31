97286d6ff81537aebe83054cb8d6e2e42c9cc6bd
safarovqodirjon <qodirjonsafaroff@gmail.com>
2023-05-31 22:23:18 +0500

    prolem with satic-2

--- pms/settings.py
+++ pms/settings.py
@@ -122,7 +122,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-
 AUTH_USER_MODEL = 'authentication.CustomUser'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
